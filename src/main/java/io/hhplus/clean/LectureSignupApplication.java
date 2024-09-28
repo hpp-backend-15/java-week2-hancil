c12f6504f837f04b972ad107de9fe1aba77c9d20
THIRSTY <dev.thirsty92@gmail.com>
2024-09-28 20:49:24 +0900

    init

---
+++ src/main/java/io/hhplus/clean/LectureSignupApplication.java
@@ -0,0 +1,13 @@
+package io.hhplus.clean;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LectureSignupApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LectureSignupApplication.class, args);
+    }
+
+}
